76f1e31eb528fc339e807acb7cc06daa67c08496
jalkal <jalcalde@gmail.com>
2017-05-28 23:54:29 +0200

    Step 3

---
+++ customer-service-no-boot/src/main/java/com/jalkal/noboot/config/com/jalkal/noboot/SpringWebAppInitializer.java
@@ -0,0 +1,31 @@
+package com.jalkal.noboot.config.com.jalkal.noboot;
+
+import com.jalkal.noboot.config.AppConfig;
+import com.jalkal.noboot.config.WebMvcConfig;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+
+public class SpringWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses()
+    {
+        return new Class<?>[] { AppConfig.class};
+    }
+    @Override
+    protected Class<?>[] getServletConfigClasses()
+    {
+        return new Class<?>[] { WebMvcConfig.class };
+    }
+    @Override
+    protected String[] getServletMappings()
+    {
+        return new String[] { "/" };
+    }
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[]{ new OpenEntityManagerInViewFilter() };
+    }
+}
